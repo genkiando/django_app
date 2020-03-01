409e90640fe715682ece7878d9ec30f03bf967ba
genki <genkiando0517@yahoo.co.jp>
2020-03-02 01:16:46 +0900

    new file

--- SecondXight_project/testapp/forms.py
+++ SecondXight_project/testapp/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Document
  
-class DocumentForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ( 'csv_file', ) 
+# class DocumentForm(forms.ModelForm):
+#     class Meta:
+#         model = Document
+#         fields = ( 'csv_file', ) 
 
+class DocumentForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField() 
--- SecondXight_project/testapp/views.py
+++ SecondXight_project/testapp/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Question
+from .models import Question, Document
 from django.core.files.storage import FileSystemStorage
 
 from django.shortcuts import render,redirect
 from .forms import DocumentForm
-from .models import Document
+
+import pandas as pd
+import os
+# import numpy as np
 
 
 def index(request):
@@ -23,14 +26,50 @@ def index(request):
 #     return HttpResponse(response % result_id)
 
  
+# def model_form_upload(request):
+#     if request.method == 'POST':                            
+#         form = DocumentForm(request.POST, request.FILES) 
+#         if form.is_valid():                                 
+#             form.save()
+#             return redirect('index')                        
+#     else:
+#         form = DocumentForm()
+    
+#     return render(request, 'testapp/index.html', {
+#         'form': form,
+#     })
+
+
+
 def model_form_upload(request):
     if request.method == 'POST':                            
         form = DocumentForm(request.POST, request.FILES) 
+
         if form.is_valid():                                 
-            form.save()
-            return redirect('index')                        
+            file_obj = request.FILES["file"]
+            file_path = 'uploadfile/file/' + file_obj.name
+            df = pd.read_csv(file_path,encoding="SHIFT-JIS")
+            df_ = df.head()
+            table = df_.to_html()
+            print(df_)
+            
+            test_text = "アップロードされました"
+            context = {
+              'test_text': test_text,
+              'file_path': file_path,
+              'df': table,
+              'file_obj': file_obj,
+            }
+            return render(request, 'testapp/index.html', context)                       
     else:
-        form = DocumentForm()
+        form = DocumentForm()   
     return render(request, 'testapp/index.html', {
-        'form': form
+        'form': form,
     })
+
+
+
+# def handle_uploaded_file(f):
+#     with open('some/file/name.txt', 'wb+') as destination:
+#         for chunk in f.chunks():
+#             destination.write(chunk)
